ff796eeddd4d6bdcee8090125aaa68d0a2fa1801
ZoyaZubair <z.zoya@gmail.com>
2024-07-21 01:19:25 +0530

    changed

--- routes/course.js
+++ routes/course.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllCourses, getSingleCourse , fetchLectures ,fetchLecture, getMyCourses, checkout} from "../controllers/course.js";
+import { getAllCourses, getSingleCourse , fetchLectures ,fetchLecture, getMyCourses, checkout, paymentVerification} from "../controllers/course.js";
 import { isAuth } from "../middlewares/isAuth.js";
 
 const router = express.Router();
@@ -10,5 +10,6 @@ router.get("/lectures/:id", isAuth, fetchLectures);
 router.get("/lecture/:id", isAuth, fetchLecture);
 router.get("/mycourse", isAuth, getMyCourses);
 router.post("/course/checkout/:id", isAuth, checkout);
+router.post("/verification/:id", isAuth, paymentVerification);
 
 export default router;
